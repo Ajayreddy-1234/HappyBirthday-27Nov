1ffd01131dfaf11bbdfdd22e49b4765deb532c18
Ajay <gajulapallyajay@gmail.com>
2021-10-21 22:56:27 +0530

    Initial Commit for Happy

---
+++ HappyBirthday.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+         
+    {
+        // Happy
+        for(int i=0;i<=50;i++){
+           if(i==5||i==9||i==15||i==17||i==23||
+           i==25||i==27||i==31||i==33||i==35||
+           i==39||i==43){
+               cout<<u8"\U0001F60A";
+           }else{
+               cout<<" ";
+           }
+        }
+        cout<<"\n";
+        for(int i=0;i<=50;i++){
+           if(i==5||i==9||i==13||i==19||i==23||
+           i==27||i==31||i==35
+           ||i==39||i==43){
+               cout<<u8"\U0001F60A";
+           }else{
+               cout<<" ";
+           }
+        }
+        cout<<"\n";
+        for(int i=0;i<=50;i++){
+            if(i==5||i==7||i==9||i==13||
+            i==15||i==17||i==19||i==23||
+            i==25||i==27||i==31||i==33||
+            i==35||i==39||i==41||i==43){
+                cout<<u8"\U0001F60A";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+        for(int i=0;i<=50;i++){
+            if(i==5||i==9||i==13||
+            i==19||i==23||i==31||i==41){
+                cout<<u8"\U0001F60A";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+        for(int i=0;i<=50;i++){
+            if(i==5||i==9||i==13||
+            i==19||i==23||i==31||i==41){
+                cout<<u8"\U0001F60A";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+    {  
+        // Birthday
+
+
+    }
+    
+
+
+
+
+
+
+
+    return 0;
+}
